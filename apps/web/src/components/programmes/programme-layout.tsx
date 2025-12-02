d2a0b4a198904a7fa22e9666788d8b74ee6eabb7
karannaik503 <karannaik503@gmail.com>
2025-12-03 01:47:23 +0530

    programmes page

---
+++ apps/web/src/components/programmes/programme-layout.tsx
@@ -0,0 +1,23 @@
+// src/components/programmes/programme-layout.tsx
+"use client";
+
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
+import { ProgrammeNav } from "./programme-nav";
+import { ReactNode } from "react";
+
+interface ProgrammeLayoutProps {
+  children: ReactNode;
+  currentProgramme: "ug" | "pg" | "mba" | "mca" | "phd";
+}
+
+export function ProgrammeLayout({ children, currentProgramme }: ProgrammeLayoutProps) {
+  return (
+    <div className="min-h-screen bg-stone-50">
+      <Header />
+      <ProgrammeNav currentProgramme={currentProgramme} />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+}
