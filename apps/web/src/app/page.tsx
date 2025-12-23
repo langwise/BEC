6f3c26aebeafd50f5bc60edf89a57516fc4add4a
Nikhil Karthik <nikhilkarthik241103@gmail.com>
2025-12-23 15:32:27 +0530

    feat: updated home and institute pages

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,23 +4,23 @@ import { ProgrammesSection } from "@/components/home/programmes-section";
 import { StatsSection } from "@/components/home/stats-section";
 import { HeroBanner } from "@/components/home/hero-banner";
 import { CampusLifeSection } from "@/components/home/campus-life-section";
-import { SocialMediaSidebar } from "@/components/home/social-media-sidebar";
 import { PlacementsSection } from "@/components/home/placements-section";
-import { NewsAnnouncementsSection } from "@/components/home/news-announcements-section";
 import { AlumniTestimonialsSection } from "@/components/home/alumni-testimonials";
+import { FollowUsSection } from "@/components/home/follow-us-section";
+import { AboutSection } from "@/components/home/about-section";
 
 export default function Home() {
   return (
     <main>
-      <SocialMediaSidebar />
       <div className="relative [clip-path:inset(0)]">
         <HeroBanner />
-        <NewsAnnouncementsSection />
-        <StatsSection />
-        <PlacementsSection />
-        <CampusLifeSection />
-        <AlumniTestimonialsSection />
+        <FollowUsSection />
+        <AboutSection />
         <ProgrammesSection />
+        <PlacementsSection />
+        <AlumniTestimonialsSection />
+        <StatsSection />
+        <CampusLifeSection />
       </div>
     </main>
   );
